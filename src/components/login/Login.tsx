b4e9da223a7c1799fa5ef76ca293a3964ba4a7db
PriscilaFabreteCandido <priscilafabrete@gmail.com>
2025-10-21 18:11:41 -0300

    fix: remover shadow

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -58,7 +58,7 @@ export const LoginComponent = () => {
           form={form}
           onFinish={onFinish}
           layout="vertical"
-          className="w-full max-w-md bg-white p-6 md:p-8 rounded-2xl shadow-sm"
+          className="w-full max-w-md bg-white p-6 md:p-8  shadow-sm"
         >
           <Form.Item
             label="Email"
